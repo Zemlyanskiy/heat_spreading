9ca2aa1c484a8ef8915325c1f1188f61d4cba406
Zemlyanskiy <nikita.nikita.000035@gmail.com>
2017-12-15 21:07:39 +0300

    Add update for .c and output

--- src/zemlyansky/heat_spreading.c
+++ src/zemlyansky/heat_spreading.c
@@ -2,6 +2,18 @@
 #include <math.h>
 #include <stdlib.h>
 
+int Output(char* DATA,char* mode,double* arr,int length) {
+  FILE* file = fopen(DATA, mode);
+  if (file == NULL)
+		printf("Can`t open file\n");
+	else {
+		for (int i = 0; i < length; i++)
+			fprintf(file, "%f ", arr[i]);
+		fprintf(file, "\n");
+		fclose(file);
+	}
+}
+
 int main() {
     FILE *fp = fopen("input1.txt", "r");
 
@@ -55,13 +67,8 @@ int main() {
   	count = 0;
     //Writing
     //First step
-    fp = fopen("input1.txt","a");
-    fprintf(fp, "\n");
-    for (int i = 0; i < Lx; i++) {
-      fprintf(fp, "%f ", points[i]);
-      //printf("%lf\n", points[i]);
-    }
-    fprintf(fp, "\n");
+    fprintf(fp,"\n");
+    Output("input1.txt", "a", points, Lx);
     //Next steps
     for (double time = t; time <= T; time += deltaT, count++) {
   		for (int i = 0; i < Lx; i++)
@@ -69,7 +76,7 @@ int main() {
   		for (int i = 1; i < Lx - 1; i++) {
   			points[i] = prev[i] + Sigma*deltaT*(prev[i - 1] - 2 * prev[i] + prev[i + 1]) / pow(step, 2);
         if (count%out_count == 0) {
-          fprintf(fp, "%f ", points[i]);
+          Output("input1.txt", "a", points, Lx);
         }
       }
     }
