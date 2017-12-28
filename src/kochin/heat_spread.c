37a7dc1d6647c5bd2ba3761efd68c08303afa909
ivankochin <ivan.kochin@inbox.ru>
2017-12-29 01:54:06 +0300

    change input

--- src/kochin/heat_spread.c
+++ src/kochin/heat_spread.c
@@ -1,6 +1,11 @@
 ﻿#include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define EILER 0;
+#define RUNGE_KUTTA 1;
+#define START_STRING 2;
 
 struct input {
 	double t;//START TIME (t)
@@ -11,13 +16,19 @@ struct input {
 	int NumPoints; // NUMBER OF MEASUREMENTS (LX)
 	double Sigma; //THE COEFFICIENT OF QUATION  (SIGMA)
 	double deltaOut;//DELTA OF OUTPUT(deltaOUT)
+	double* values; //LAST ARRAY WITH VALUES
 };
 
-struct input ReadInput(char* PATH) {
+struct input ReadInput(char* PATH,int mode) {
 	struct input temp;
 	FILE *file;
+
+	char* token;
+	int pos;
+	double buf;
+
 	fopen_s(&file,PATH, "r");
-	char string[100];
+	char string[5000];
 	if (file == NULL) {
 		printf("CANT FIND FILE\n");
 	}
@@ -26,7 +37,7 @@ struct input ReadInput(char* PATH) {
 		for (int i = 0; fgets(string, sizeof(string), file) != NULL; i++)
 			switch (i) {
 			case 0:
-			{
+			{ 
 				sscanf_s(string, "t=%lf", &temp.t);
 				printf("t = %f\n", temp.t);
 				break;
@@ -59,6 +70,10 @@ struct input ReadInput(char* PATH) {
 			{
 				sscanf_s(string, "Lx=%d", &temp.NumPoints);
 				printf("NumPoints = %d\n", temp.NumPoints);
+				temp.values = malloc(sizeof(double)*temp.NumPoints);
+				for (int i = 0; i < temp.NumPoints; i++) {
+					temp.values[i] = 0;
+				}
 				break;
 			}
 			case 6:
@@ -74,30 +89,23 @@ struct input ReadInput(char* PATH) {
 				break;
 			}
 			default:
+				if (mode == 2)
 				printf("WRONG LINE INPUT NUMBER\n");
+				else {
+					pos = 0;
+					token = strtok(string, " ");
+					while (token != NULL)
+					{
+						buf = strtof(token,NULL);
+						temp.values[pos] = buf;
+						token = strtok(NULL, " ");
+						pos++;
+					}
+				}
 			}
 	}
 	fclose(file);
 	return temp;
-}
-
-int Output(struct input temp, double** points, unsigned int len) {
-	printf("t = %f\n", temp.t);
-	printf("T = %f\n", temp.T);
-	printf("deltaT = %f\n", temp.deltaT);
-	printf("Xmin = %f\n", temp.Xmin);
-	printf("Xmax = %f\n", temp.Xmax);
-	printf("NumPoints = %d\n", temp.NumPoints);
-	printf("Sigma = %f\n", temp.Sigma);
-	printf("deltaOut=%f\n", temp.deltaOut);
-	if (points != NULL) {
-		for (int i = 0; i < len; i++)
-			printf("%f ", points[0][i]);
-		printf("\n");
-		for (int i = 0; i < len; i++)
-			printf("%f ", points[1][i]);
-		printf("\n");
-	}
 }
 
 double CalcFunc(double point) {
@@ -109,55 +117,129 @@ double CalcFunc(double point) {
 	return result;
 }
 
-int OutputArr(char* PATH, char* mode, double* arr, int length) {
+int OutputArr(char* PATH, double* arr, int length) {
 	FILE* file;
-	fopen_s(&file, PATH, mode);
+	fopen_s(&file, PATH, "a+");
 	if (file == NULL)
 		printf("Cant open file\n");
 	else {
+		fprintf(file, "\n");
 		for (int i = 0; i < length; i++)
 			fprintf(file, "%f ", arr[i]);
-		fprintf(file, "\n");
 		fclose(file);
 	}
 
 }
 
-int main() {
+char* FloatToString(double in) {
+	int IntPart = (int)in;
+	double DoublePart = in - IntPart;
+	int size = 1;//char #','
+	int buf;
+	if (IntPart == 0)
+		size++;
+	if (DoublePart == 0)
+		size++;
+	while (IntPart > 0) {
+		size++;
+		IntPart /= 10;
+	}
+	while (DoublePart > 0) {
+		DoublePart *= 10;
+		buf = (int)DoublePart;
+		DoublePart -= buf;
+		size++;
+	}
 
+}
+
+int OutputCurrentTime(char* PATH, double currT) {
+	FILE* file;
+	int ERR_CODE,num_of_space=15-8;
+	int tmp=(int)currT;
+	fopen_s(&file, PATH, "r+");
+	if (file == NULL)
+		printf("Cant open file\n");
+	else {
+		while (tmp > 10) {
+			tmp /= 10;
+			num_of_space--;
+		}
+
+		fseek(file, 0, SEEK_SET);
+		fputs("t=", file);
+		for (int i=0;i<num_of_space;i++)
+			fputs(" ", file);
+		fprintf(file,"%f",currT);
+		ERR_CODE = ftell(file);
+		fclose(file);
+	}
+	return 0;
+}
+
+int main(int argc, char *argv[]) {
+	char* inputFile;
+	int PROGRAMM_MODE = EILER;
+	if (argc == 2) {
+		inputFile = argv[1];
+	}
+	else if (argc == 3) {
+		inputFile = argv[1];
+		if (argv[2] == 'e')
+			PROGRAMM_MODE = EILER;
+		if (argv[2] == 'r')
+			PROGRAMM_MODE = RUNGE_KUTTA;
+		if (argv[2] == 's')
+			PROGRAMM_MODE = START_STRING;
+	}
+	else {
+		printf("Incorrect number of arguments");
+		return 4;
+	}
 	//READ INPUT DATA AND CREATE START SITUATION
 	struct input data;//struct for input file
 	double* points;//array for caclulate results
 	double* prev;//array to previos state
 	double posX; //var for calculate first state
 	double step;
+	double OutTime = 0;
 	int numOutputs;
 	int outCoun;
 	int count;
-	data = ReadInput("input1.txt");
-	step = (data.Xmax - data.Xmin) / data.NumPoints;
+	data = ReadInput(inputFile, PROGRAMM_MODE);
 	points = (double*)malloc(sizeof(double)*data.NumPoints);
-	prev = (double*)malloc(sizeof(double)*data.NumPoints);
 	posX = data.Xmin;
-	for (int i = 0; i < data.NumPoints; i++) {
-		points[i] = CalcFunc(posX);
-		posX += step;
+	step = (data.Xmax - data.Xmin) / data.NumPoints;
+	
+	
+	if (PROGRAMM_MODE == 2) {
+		for (int i = 0; i < data.NumPoints; i++) {
+			points[i] = CalcFunc(posX);
+			posX += step;
+		}
+		//OUTPUT FIRST RESULTS
+		OutputArr(inputFile, points, data.NumPoints);
 	}
-
-	//OUTPUT FIRST RESULTS
-	OutputArr("input1.txt", "a", points, data.NumPoints);
-	//START PROCESS
-	numOutputs =(int) (data.T - data.t) / data.deltaOut;
-	outCoun = (int)(data.T - data.t) / data.deltaT;
-	outCoun = outCoun / numOutputs;
-	count = 0;
-	for (double time = data.t; time <= data.T; time += data.deltaT, count++) {
-		for (int i = 0; i < data.NumPoints; i++)
-			prev[i] = points[i];
-		for (int i = 1; i < data.NumPoints - 1; i++)
-			points[i] = prev[i] + data.Sigma*data.deltaT*(prev[i - 1] - 2 * prev[i] + prev[i + 1]) / pow(step, 2);
-		if (count%outCoun == 0) {
-			OutputArr("input1.txt", "a", points, data.NumPoints);
+	else
+	{	//START PROCESS(EILER)
+		for (int i = 0; i < data.NumPoints; i++) {
+			points[i] = data.values[i];
+		}
+		prev = (double*)malloc(sizeof(double)*data.NumPoints);
+		numOutputs = (int)(data.T - data.t) / data.deltaOut;
+		outCoun = (int)(data.T - data.t) / data.deltaT;
+		outCoun = outCoun / numOutputs;
+		count = 0;
+		for (double time = data.t; time <= data.T; time += data.deltaT, count++) {
+			for (int i = 0; i < data.NumPoints; i++)
+				prev[i] = points[i];//make pointers swap
+			for (int i = 1; i < data.NumPoints - 1; i++)
+				points[i] = prev[i] + data.Sigma*data.deltaT*(prev[i - 1] - 2 * prev[i] + prev[i + 1]) / pow(step, 2);
+			if (count%outCoun == 0) {
+				OutputArr(inputFile, points, data.NumPoints);//if ERROR will be after this line, numbers of lines and t parameter will be wrong
+				OutputCurrentTime(inputFile, OutTime);
+				OutTime += data.deltaOut;
+			}
 		}
 	}
 	printf("\n\nEND\n\n");
