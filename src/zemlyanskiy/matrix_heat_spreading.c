2b6e12a8bc6799334d997b721fca65593ed4f18a
Zemlyanskiy Nikita <nikita.nikita.000035@gmail.com>
2018-04-18 03:47:44 +0300

    Added CRS-format

--- src/zemlyanskiy/matrix_heat_spreading.c
+++ src/zemlyanskiy/matrix_heat_spreading.c
@@ -111,6 +111,17 @@ int Output(char* DATA,char* mode,double* arr,int length) {
 	}
 }
 
+double* MultiCSR(double* arr,double* csr_values,int* first_line, int* columns, struct input_data temp) {
+  double* composition = (double*)malloc(sizeof(double)*temp.Lx);
+  #pragma omp parallel for
+  for(int i = 0; i < temp.Lx; i++) {
+    composition[i] = 0;
+    for(int j = first_line[i]; j < first_line[i + 1]; j++)
+      composition[i]+= csr_values[j] * arr[columns[j]];
+  }
+  return composition;
+}
+
 int main(int argc, char *argv[]) {
     char* file = argv[1];
     int prog_mode;
@@ -129,10 +140,15 @@ int main(int argc, char *argv[]) {
     double posX;
     int numbers_of_out;
     int out_count;
+    int max_count;
     int count;
     double CurTime = 0;
     double start, end;
     double Restime;
+    //variables for CSR
+    int* first_line;
+    int* columns;
+    double* csr_values;
 
     //Reading data
     struct input_data data = Read(file, prog_mode);
@@ -160,11 +176,27 @@ int main(int argc, char *argv[]) {
           cur_points[i] = data.values[i];
         }
       }
+      //Initialize CSR Matrix
+      first_line = (int*)malloc(sizeof(int)*data.Lx + 1);
+      columns = (int*)malloc(sizeof(int)*(data.Lx - 2) * 3);
+      csr_values = (double*)malloc(sizeof(double)*(data.Lx - 2) * 3);
+      for(int i = 0; i < data.Lx - 2; i++) {
+        csr_values[i * 3] = data.Sigma * data.deltaT * quad_step;
+        csr_values[i * 3 + 1] = 1 - 2 * data.Sigma * data.deltaT * quad_step;
+        csr_values[i * 3 + 2] = data.Sigma * data.deltaT * quad_step;
+        first_line[i + 1] = i * 3;
+        columns[i * 3] = i;
+        columns[i * 3 + 1] = i + 1;
+        columns[i * 3 + 2] = i + 2;
+      }
+      first_line[0] = first_line[1];
+      first_line[data.Lx - 1] = first_line[data.Lx - 2];
+      first_line[data.Lx] = (data.Lx-2) * 3;
         //Calculate data
         prev_points = (double*)malloc(sizeof(double)*data.Lx);
         numbers_of_out = (int) (data.T - data.t) / data.deltaOut;
-      	out_count = (int)(data.T - data.t) / data.deltaT;
-      	out_count = out_count / numbers_of_out;
+      	max_count = (int)(data.T - data.t) / data.deltaT;
+      	out_count = max_count / numbers_of_out;
       	count = 0;
         if(prog_mode == 0) {//Euler
           omp_set_num_threads(num_threads);
@@ -175,10 +207,13 @@ int main(int argc, char *argv[]) {
               #pragma omp for
               for (int i = 0; i < data.Lx; i++)
                 prev_points[i] = cur_points[i];
-              #pragma omp for
+
+              cur_points = MultiCSR(prev_points,csr_values,first_line,columns,data);
+
+            /*  #pragma omp for
               for (int i = 1; i < data.Lx - 1; i++)
                 cur_points[i] = prev_points[i] + data.Sigma*data.deltaT*(prev_points[i - 1]
-                                                - 2 * prev_points[i] + prev_points[i + 1]) * quad_step;
+                                                - 2 * prev_points[i] + prev_points[i + 1]) * quad_step;*/
             }
             if (count%out_count == 0) {
               Output(file, "a", cur_points, data.Lx);
@@ -212,7 +247,25 @@ int main(int argc, char *argv[]) {
               #pragma omp for
     				  for (int i = 0; i < data.Lx; i++)
     					   prev_points[i] = cur_points[i];
+
+              k1 = MultiCSR(prev_points,csr_values,first_line,columns,data);
+
               #pragma omp for
+              for(int i = 0; i < data.Lx;i++)
+                  middle[i] = (prev_points[i] + k1[i]) * 0.5;
+
+              k2 = MultiCSR(middle,csr_values,first_line,columns,data);
+              #pragma omp for
+              for(int i = 0; i < data.Lx;i++)
+                  middle[i] = (prev_points[i] + k2[i]) * 0.5;
+
+              k3 = MultiCSR(middle,csr_values,first_line,columns,data);
+              #pragma omp for
+              for(int i = 0; i < data.Lx;i++)
+                  middle[i] = prev_points[i];
+
+              k4 = MultiCSR(middle,csr_values,first_line,columns,data);
+              /*#pragma omp for
               for(int i = 1; i < data.Lx - 1; i++) {
                 k1[i] = data.Sigma*data.deltaT*(prev_points[i - 1] - 2 * prev_points[i] + prev_points[i + 1]) * quad_step;
 
@@ -222,12 +275,15 @@ int main(int argc, char *argv[]) {
 
                 k4[i] = data.Sigma*data.deltaT*((prev_points[i - 1] + k3[i - 1]) - 2 * (prev_points[i] + k3[i]) + (prev_points[i + 1] + k3[i + 1])) * quad_step;
 
+                middle[i] = (k1[i] + 2 * k2[i] + 2 * k3[i] + k4[i]) / 6;
+              }*/
+              #pragma omp for
+              for(int i = 1; i < data.Lx - 1; i++)
                 middle[i] = (k1[i] + 2 * k2[i] + 2 * k3[i] + k4[i]) * 0.166666667;
-              }
               // Calculate next steps
               #pragma omp for
-              for(int i = 0; i < data.Lx; i++)
-                cur_points[i] = prev_points[i] + middle[i];
+              for(int i = 1; i < data.Lx - 1; i++)
+                cur_points[i] = middle[i];
           }
           if (count%out_count == 0) {
             Output(file, "a", cur_points, data.Lx);
